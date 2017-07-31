f0242179ddfaf3682c2159fde364af86e55a4f43
Adeniji Charles ayodipupo <adenijiayocharles@gmail.com>
2017-07-31 21:35:35 +0100

    Finished 2nd draft

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-document.getElementById("submit").addEventListener("click", function(){
-	var text = document.getElementById("list").value;
+var textBox = document.getElementById("submit");
+textBox.addEventListener("click", function(){
+	var text = document.getElementById("list");
 	var listNode = document.createElement("li");
-	var textNode = document.createTextNode(text);
+	var textNode = document.createTextNode(text.value);
 	listNode.appendChild(textNode);
+	listNode.classList = "li-class";
+	listNode.setAttribute("onclick","removeTodo(this);");
 
 	var ulTextNode = document.getElementById("todo");
 	ulTextNode.appendChild(listNode);
+	text.value = "";
 });
+
+function removeTodo(arg){
+	arg.style.display = 'none';
+}
