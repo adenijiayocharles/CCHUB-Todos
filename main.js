60bdff64b9ce1bfb08b6e073df7b5b46a170f17d
Adeniji Charles ayodipupo <adenijiayocharles@gmail.com>
2017-07-31 21:41:37 +0100

    Added error checking

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 var textBox = document.getElementById("submit");
 textBox.addEventListener("click", function(){
 	var text = document.getElementById("list");
-	var listNode = document.createElement("li");
-	var textNode = document.createTextNode(text.value);
-	listNode.appendChild(textNode);
-	listNode.classList = "li-class";
-	listNode.setAttribute("onclick","removeTodo(this);");
+	var textValue = text.value;
 
-	var ulTextNode = document.getElementById("todo");
-	ulTextNode.appendChild(listNode);
-	text.value = "";
+	if(textValue === "" || textValue === " "){
+		alert("Text can not be empty");
+		return false;
+	}else{
+		var listNode = document.createElement("li");
+		var textNode = document.createTextNode(textValue);
+		listNode.appendChild(textNode);
+		listNode.classList = "li-class";
+		listNode.setAttribute("onclick","removeTodo(this);");
+
+		var ulTextNode = document.getElementById("todo");
+		ulTextNode.appendChild(listNode);
+		document.getElementById("instruction").style.display = 'block';
+		text.value = "";		
+	}
 });
 
 function removeTodo(arg){
